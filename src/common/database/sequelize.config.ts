4f4c70b1bc6863e4b39f30fc6986025690a36f1f
r.albhaisi <r.albhaisi@cloq.com.br>
2024-11-24 13:21:26 +0200

    Updated sequelize.config Added registerModels Fn to make it more usable and to group --> now we can add models dynamically

--- src/common/database/sequelize.config.ts
+++ src/common/database/sequelize.config.ts
@@ -1,14 +1,24 @@
 import { Sequelize } from "sequelize-typescript";
+import { Dialect } from "sequelize";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
-export const sequelize = new Sequelize({
-  dialect: "postgres",
+const sequelizeConfig = new Sequelize({
+  dialect: (process.env.DB_DIALECT as Dialect) || "postgres",
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT, 10) || 5432,
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "fakePassword",
   database: process.env.DB_NAME || "EryndorRecordsTest",
-  //models: [UserModel], // Register your Sequelize models here
   logging: false, // Enable SQL query logging for debugging
+});
+
+export const registerModels = (models: any[]): void => {
+  sequelize.addModels(models);
+};
+
+export const sequelize = new Sequelize({
+  ...sequelizeConfig,
+  models: [], // Models will be registered dynamically
 });
